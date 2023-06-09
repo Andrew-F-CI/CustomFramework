9ce94b3bf876624abb7f52bc4c0c2b685c86fda5
Andrew F <afairchild@cloudingenuity.com>
2023-06-09 12:40:48 -0500

    first commit

---
+++ Sources/CustomFrameworkPackage/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Andrew Fairchild on 6/9/23.
+//
+
+import Foundation
+
+
+public struct SDK {
+    public static func  doSomeWork() {
+        print("Doing some work...")
+    }
+}
