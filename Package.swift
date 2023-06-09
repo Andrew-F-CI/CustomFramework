32f9c195308d4dc8d98e0cd9e241c865f994c3cd
Andrew F <afairchild@cloudingenuity.com>
2023-06-09 13:50:51 -0500

    0.0.6

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "CustomFrameworkPackage",
+    name: "CustomFramework",
     platforms: [
       .macOS(.v12), .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CustomFramework",
+            name: "CustomFrameworkPackage",
             targets: ["CustomFramework"]),
     ],
     dependencies: [
